ba9ecf3f27e14b895a1288df95a816294e02ef20
utkarmis <utkarsh.a.mishra@capgemini.com>
2022-06-13 20:51:36 +0530

    UPDATE : Code Update in StringCalculator class. ADD : Added configuration methods for string calculations like add(), convertToInt(),splitNumbers() and totalSum().

--- app/src/main/java/com/example/harmanstringcalculator/StringCalculator.java
+++ app/src/main/java/com/example/harmanstringcalculator/StringCalculator.java
@@ -1,4 +1,50 @@
 package com.example.harmanstringcalculator;
 
 public class StringCalculator {
+    //This method will handle various cases for string calculation
+    public static int add(String text){
+        if(text.equals("")){
+            return 0;
+        }
+        else{
+            String delimiter = ",";
+            if(text.matches("//(.*)\n(.*)")){
+                delimiter = Character.toString(text.charAt(2));
+                text = text.substring(4);
+            }
+
+            String[] numList = splitNumbers(text, delimiter + "|\n");
+            return totalSum(numList);
+        }
+    }
+
+    private static int convertToInt(String number){
+        return Integer.parseInt(number);
+    }
+
+    private static String[] splitNumbers(String numbers, String divider){
+        return numbers.split(divider);
+    }
+
+    private static int totalSum(String[] numbers){
+        int sumTotal = 0;
+        StringBuilder negString = new StringBuilder();
+
+        for(String number : numbers){
+            if(convertToInt(number) < 0){
+                if(negString.toString().equals(""))
+                    negString = new StringBuilder(number);
+                else
+                    negString.append(",").append(number);
+            }
+            if(convertToInt(number) < 1000)
+                sumTotal += convertToInt(number);
+        }
+
+        if(!negString.toString().equals("")){
+            throw new IllegalArgumentException("Negatives not allowed: " + negString);
+        }
+
+        return sumTotal;
+    }
 }
