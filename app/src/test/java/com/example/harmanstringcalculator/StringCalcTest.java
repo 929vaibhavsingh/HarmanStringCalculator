c3c97d0ebdb76ccc36fc7d862270c1f84500cbfe
utkarmis <utkarsh.a.mishra@capgemini.com>
2022-06-13 21:00:56 +0530

    UPDATE : Code Update in StringCalcTest class. added unit test case methods.

--- app/src/test/java/com/example/harmanstringcalculator/StringCalcTest.java
+++ app/src/test/java/com/example/harmanstringcalculator/StringCalcTest.java
@@ -1,5 +1,34 @@
 package com.example.harmanstringcalculator;
 
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class StringCalcTest {
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("com.example.harmanstringcalculator.StringCalcTest");
+    }
+    //Test case for empty string
+    @Test
+    public void testForEmptyString() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+    //Test case for 1 string
+    @Test
+    public void testFor1Number() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
+    //Test case for 2 string
+    @Test
+    public void testFor2Numbers(){
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
+    //Test case for 3 string
+    @Test
+    public void testFor3Numbers(){
+        assertEquals(6, StringCalculator.add("1,2,3"));
+    }
+
+
 }
